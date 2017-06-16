02beb3c7bbe6c92538b77693c89a01e7c46888a4
Hao <haonhatvinh@gmail.com>
2017-06-16 14:23:05 +0700

    app cua toi

--- App1/ViewController.swift
+++ App1/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        APIRequest.arrNews { (arrNews) in
+            for i in 0..<arrNews.count{
+                let news: NewsObject = arrNews[i] 
+                print(news.content!)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
